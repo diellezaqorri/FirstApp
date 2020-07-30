337486e0e0cac2be5c936bc5a5dad22c80e6da7f
Dielleza <dieellza.q@hotmail.com>
2020-07-30 14:11:31 +0200

    whatever

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,60 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Jepni numrin tuaj:");
-        int a = input.nextInt();
+        //// Krijo funksionin qe llogarit shumen e numrave nga nje numer n deri ne nje numer m. Rezultati te kthehet
+        //// dhe te ruhet ne nje variabel. Ai rezultat le te zbritet me rezultatin e funksionit qe kthen shumezimin e
+        //// numrit c dhe numrit d. Rezultati ne fund te printohet duke perdorur metoden void
 
-        System.out.println("Numri juaj eshte:" + a);
-        System.out.println("Numri i kundert me inputin tuaj eshte:" + numriKundert(a));
+        while (true) {
+            Scanner skaneri = new Scanner(System.in);
+            System.out.println("Pershendetje");
+            System.out.println("Jepni numrin e pare per tu mbledhur");
+            int n = skaneri.nextInt();
+            System.out.println("Jepni numrin e dyte per tu mbledhur");
+            int m = skaneri.nextInt();
+            System.out.println("Jepni numrin e pare per tu shumzuar");
+            int c = skaneri.nextInt();
+            System.out.println("Jepni numrin e dyte per tu shumzuar");
+            int d = skaneri.nextInt();
+
+            int mbledhja = mbledhja(n, m);
+            int prodhimi = prodhimi(c, d);
+            rezultati2(mbledhja, prodhimi);
+            System.out.println("Shtypni 1 per te vazhduar, numer tjeter per te anuluar ");
+            int vazhdo = skaneri.nextInt();
+            if(vazhdo != 1) {
+                break;
+            }
+        }
 
 
     }
 
-    static int numriKundert(int n) {
-        int numritjeter = 0;
-        if (n != 0)
-            numritjeter = n * (-1);
+    static int mbledhja(int n, int m) {
 
+        int rezultati = 0;
 
-        return numritjeter;
+        while (n <= m)
+            rezultati += n++;
+        return rezultati;
     }
+
+    static int prodhimi(int c, int d) {
+
+        int rezultati = c * d;
+        return rezultati;
+
+    }
+
+    static void rezultati2(int k, int g) {
+        int rezultatiFinal = k - g;
+
+        System.out.println("Rezultati = " + rezultatiFinal);
+
+    }
+
 
 }
-
-//krijo nje metode qe merr si parameter numrin dhe kthen numrin e kundert ,nese jep zero te kthen zero.r
-//rezutlati te kthehet
-
-// Krijo funksionin qe llogarit shumen e numrave nga nje numer n deri ne nje numer m. Rezultati te kthehet
-// dhe te ruhet ne nje variabel. Ai rezultat le te zbritet me rezultatin e funksionit qe kthen shumezimin e
-// numrit c dhe numrit d. Rezultati ne fund te printohet duke perdorur metoden void
-
-// metoda 1: int shumaNumrave(int m, int n)
-// metoda 2: int prodhimiNumrave(int c, int d)
-// metoda 3: void printoRezultatin(int shuma, int prodhimi)
-
-// si duket ne fund: "shuma - prodhimi =" + rezultati
 
 
 
